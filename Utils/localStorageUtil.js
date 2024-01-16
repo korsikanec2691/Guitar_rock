93d2d438e351d2e8c8b65b991a230a34ecd72f1e
Rustamov Rovchan <rus2691@yandex.ru>
2024-01-16 15:40:47 +0300

    update: создание localStorage #6 14.08

---
+++ Utils/localStorageUtil.js
@@ -0,0 +1,22 @@
+class LocalStorageUtil {
+    constructor() {
+        this.keyName = 'products';
+    }
+
+    getProducts() {
+        const productsLocalStorage = localStorage.getItem(this.keyName);
+        if (productsLocalStorage !== null) {
+            return JSON.parse(productsLocalStorage);
+        }
+        return [];
+    }
+
+    putProducts(id) {
+
+    }
+}
+
+const localStorageUtil = new LocalStorageUtil();
+
+const a = localStorageUtil.getProducts();
+console.log(a);
